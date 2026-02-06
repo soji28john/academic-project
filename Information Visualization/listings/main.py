86246397424ab51ae1556230a224a1116dcc6608
soji28john <143357095+soji28john@users.noreply.github.com>
2026-02-06 13:03:06 +0100

    Add files via upload

--- Information Visualization/listings/main.py
+++ Information Visualization/listings/main.py
@@ -1,1 +1,19 @@
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+from .job_listings import get_listings_component, register_listings_callbacks
 
+def get_listings_layout():
+    return dbc.Container([
+        dcc.Loading(
+            id="listings-loading",
+            type="circle",
+            color="#00BFFF",
+            fullscreen=False,
+            children=dbc.Row([
+                get_listings_component()
+            ])
+        )
+    ], fluid=True)
+
+def register_listings(app):
+    register_listings_callbacks(app)
