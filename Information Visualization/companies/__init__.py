7548e7154ff20bdf4eaad16a18f54c420526e5d4
soji28john <143357095+soji28john@users.noreply.github.com>
2026-02-06 13:00:01 +0100

    Add files via upload

---
+++ Information Visualization/companies/__init__.py
@@ -0,0 +1,9 @@
+from .main import get_companies_layout, register_company_callbacks, all_companies, default_companies
+
+
+__all__ = [
+    "get_companies_layout",
+    "register_company_callbacks",
+    "all_companies",
+    "default_companies"
+]
--- Information Visualization/companies/main.py
+++ Information Visualization/companies/main.py
@@ -1,1 +1,22 @@
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+from .companies_jobs import get_company_component, register_company_callbacks, all_companies, default_companies
 
+def get_companies_layout():
+    return html.Div([
+        dcc.Loading(
+            id="companies-loading",
+            type="circle",
+            color="#00BFFF",
+            fullscreen=False,
+            children=html.Div([
+                dbc.Row([
+                    get_company_component(),
+                ]),
+                html.Br()
+            ])
+        )
+    ])
+
+def register_companies_callbacks(app):
+    register_company_callbacks(app)
